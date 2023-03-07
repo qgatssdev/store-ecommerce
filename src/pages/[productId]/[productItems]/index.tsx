2cb19e06158062cff21cdfa3024a21e6998b5912
qgatssdev <chivoomodu@gmail.com>
2023-03-07 16:19:51 +0100

    Add product items slug

---
+++ src/pages/[productId]/[productItems]/index.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+import React from 'react';
+
+const ProductItems = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default ProductItems;
